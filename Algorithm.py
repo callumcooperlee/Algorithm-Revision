84811ecbd28af72aff41b4888d266d86167052e8
Callum Cooper Lee <2758297C@student.gla.ac.uk>
2024-01-22 23:58:25 +0000

    Algorithm class signature

---
+++ Algorithm.py
@@ -0,0 +1,10 @@
+class Algorithm:
+    def __init__(self,algorthm_name):
+        self.algorthm_name = algorthm_name
+        self.lines = self.read_file()
+
+    def read_file(self):
+        pass
+
+    def practice_algorithm(self):
+        pass
